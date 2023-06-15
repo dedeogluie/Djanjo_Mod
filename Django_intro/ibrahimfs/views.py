2ea471234e5238a04d7320df05137895647c53b0
dedeogluie <dedeoglu.ie@gmail.com>
2023-06-15 22:09:19 +0300

    feat:finished project

---
+++ Django_intro/ibrahimfs/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    return HttpResponse("MERHABA DJANGO")
+
+def home(request):
+    return HttpResponse("MERHABA DJANGO")
+
+def home(request):
+    return HttpResponse("MERHABA DJANGO")
+
+def home(request):
+    return HttpResponse("MERHABA DJANGO")
+
+def home(request):
+    return HttpResponse("MERHABA DJANGO")
+
+def home(request):
+    return HttpResponse("MERHABA DJANGO")
